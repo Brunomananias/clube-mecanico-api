89da523fa8b13ca4160d47bb844fd3b4db63e8eb
Bruno Ananias <brunomananias2009@hotmail.com>
2026-01-09 05:18:01 -0300

    corrigindo interface

--- Domain/Interfaces/IServices.cs
+++ Domain/Interfaces/IServices.cs
@@ -36,7 +36,7 @@ namespace ClubeMecanico.Application.Interfaces
     {
         Task<IEnumerable<Curso>> GetAllCursosAsync();
         Task<Curso?> GetCursoByIdAsync(int id);
-        Task<Curso> CriarCursoAsync(CriarCursoDTO cursoDto, int adminId);
+        Task<Curso> CriarCursoAsync(CriarCursoDTO cursoDto);
         Task<IEnumerable<Turma>> GetTurmasByCursoIdAsync(int cursoId);
         Task<CursoAluno> MatricularAlunoAsync(MatricularAlunoCursoDTO matriculaDto, int usuarioId);
         Task<IEnumerable<CursoAluno>> BuscarCursosAlunos(int idAluno);
